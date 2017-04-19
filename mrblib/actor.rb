e8bcca79bd427479c6e570f9a1a53775ec455f19
Asmod4n <Asmod4n@users.noreply.github.com>
2017-04-19 21:47:48 +0200

    speed up messages passing, breaks the api sadly

--- mrblib/actor.rb
+++ mrblib/actor.rb
@@ -3,6 +3,15 @@ unless ZMQ.const_defined?("Poller")
 end
 
 class Actor < ZMQ::Thread
+  include ZMQ::ThreadConstants
+
+  REMOTE_NEW = 4.freeze
+  REMOTE_SEND = 5.freeze
+  REMOTE_ASYNC = 6.freeze
+  REMOTE_ACTORS = 7.freeze
+
+  ACTORS = 3.freeze
+
   def self.new(options = {}) # this starts the background thread
     super(Actor_fn, options)
   end
@@ -11,39 +20,39 @@ class Actor < ZMQ::Thread
     if block_given?
       raise ArgumentError, "blocks cannot be migrated"
     end
-    LibZMQ.send(@pipe, {type: :remote_new, peerid: peerid, class: mrb_class, args: args}.to_msgpack, 0)
-    msg = MessagePack.unpack(@pipe.recv.to_str(true))
-    case msg[:type]
-    when :instance
-      RemoteThreadProxy.new(self, peerid, msg[:object_id])
-    when :exception
-      raise msg[:exception]
+    LibZMQ.send(@pipe, [REMOTE_NEW, peerid, mrb_class, args].to_msgpack, 0)
+    msg = MessagePack.unpack(@pipe.recv.to_str)
+    case msg[TYPE]
+    when INSTANCE
+      RemoteThreadProxy.new(self, peerid, msg[1])
+    when EXCEPTION
+      raise msg[1]
     end
   end
 
   def remote_send(peerid, object_id, method, *args)
-    LibZMQ.send(@pipe, {type: :remote_send, peerid: peerid, object_id: object_id, method: method, args: args}.to_msgpack, 0)
-    msg = MessagePack.unpack(@pipe.recv.to_str(true))
-    case msg[:type]
-    when :result
-      msg[:result]
-    when :exception
-      raise msg[:exception]
+    LibZMQ.send(@pipe, [REMOTE_SEND, peerid, object_id, method, args].to_msgpack, 0)
+    msg = MessagePack.unpack(@pipe.recv.to_str)
+    case msg[TYPE]
+    when RESULT
+      msg[1]
+    when EXCEPTION
+      raise msg[1]
     end
   end
 
   def remote_async(peerid, object_id, method, *args)
-    LibZMQ.send(@pipe, {type: :remote_async, peerid: peerid, object_id: object_id, method: method, args: args}.to_msgpack, 0)
+    LibZMQ.send(@pipe, [REMOTE_ASYNC, peerid, object_id, method, args].to_msgpack, 0)
   end
 
   def remote_actors
-    LibZMQ.send(@pipe, {type: :remote_actors}.to_msgpack, 0)
-    msg = MessagePack.unpack(@pipe.recv.to_str(true))
-    case msg[:type]
-    when :actors
-      msg[:actors]
-    when :exception
-      raise msg[:exception]
+    LibZMQ.send(@pipe, [REMOTE_ACTORS].to_msgpack, 0)
+    msg = MessagePack.unpack(@pipe.recv.to_str)
+    case msg[TYPE]
+    when ACTORS
+      msg[1]
+    when EXCEPTION
+      raise msg[1]
     end
   end
 
@@ -83,7 +92,9 @@ class Actor < ZMQ::Thread
     end
   end
 
-  class Actor_fn < ZMQ::Thread_fn
+  class Actor_fn < ZMQ::Thread::Thread_fn # this is the background thread
+    include ZMQ::ThreadConstants
+
     ROUTER_ENDPOINT = 'router_endpoint'.freeze
     ROUTER_PUBLIC_KEY = 'router_public_key'.freeze
     VERSION = '2'
@@ -99,9 +110,6 @@ class Actor < ZMQ::Thread
       @remote_server = ZMQ::Router.new
       @poller << @remote_server
       @remote_server.curve_security(type: :server, secret_key: server_keypair[:secret_key])
-      unless @remote_server.mechanism == LibZMQ::CURVE
-        raise RuntimeError, "cannot set curve security"
-      end
       @remote_server.bind(@options.fetch(:remote_server_endpoint) { sprintf('tcp://%s:*', ZMQ.network_interfaces.first) } )
       last_endpoint = @remote_server.last_endpoint
       @remote_server.identity = last_endpoint
@@ -137,9 +145,6 @@ class Actor < ZMQ::Thread
         client = ZMQ::Dealer.new
         client.curve_security(type: :client, server_key: @discovery.peer_header_value(peerid, ROUTER_PUBLIC_KEY),
           public_key: @client_keypair[:public_key], secret_key: @client_keypair[:secret_key])
-        unless client.mechanism == LibZMQ::CURVE
-          raise RuntimeError, "cannot set curve security"
-        end
         client.rcvtimeo = 120000
         client.sndtimeo = 120000
         client.connect(@discovery.peer_header_value(peerid, ROUTER_ENDPOINT))
@@ -162,41 +167,41 @@ class Actor < ZMQ::Thread
 
     def handle_pipe
       msg = @pipe.recv
-      msg_str = msg.to_str(true)
+      msg_str = msg.to_str
       if msg_str == TERM
         @interrupted = true
       else
         msg_val = MessagePack.unpack(msg_str)
         begin
-          case msg_val[:type]
-          when :new
-            instance = msg_val[:class].new(*msg_val[:args])
+          case msg_val[TYPE]
+          when NEW
+            instance = msg_val[1].new(*msg_val[2])
             id = instance.__id__
-            LibZMQ.send(@pipe, {type: :instance, object_id: id}.to_msgpack, 0)
+            LibZMQ.send(@pipe, [INSTANCE, id].to_msgpack, 0)
             @instances[id] = instance
-          when :send
-            LibZMQ.send(@pipe, {type: :result, result: @instances.fetch(msg_val[:object_id]).__send__(msg_val[:method], *msg_val[:args])}.to_msgpack, 0)
-          when :async
-            if (instance = @instances[msg_val[:object_id]])
+          when SEND
+            LibZMQ.send(@pipe, [RESULT, @instances.fetch(msg_val[1]).__send__(msg_val[2], *msg_val[3])].to_msgpack, 0)
+          when ASYNC
+            if (instance = @instances[msg_val[1]])
               begin
-                instance.__send__(msg_val[:method], *msg_val[:args])
+                instance.__send__(msg_val[2], *msg_val[3])
               rescue => e
                 ZMQ.logger.crash(e)
               end
             end
-          when :finalize
-            @instances.delete(msg_val[:object_id])
-          when :remote_new, :remote_send
-            client = remote_client(msg_val[:peerid])
+          when FINALIZE
+            @instances.delete(msg_val[1])
+          when REMOTE_NEW, REMOTE_SEND
+            client = remote_client(msg_val[1])
             LibZMQ.msg_send(msg, client, 0)
             LibZMQ.msg_send(client.recv, @pipe, 0)
-          when :remote_async
-            LibZMQ.msg_send(msg, remote_client(msg_val[:peerid]), 0)
-          when :remote_actors
-            LibZMQ.send(@pipe, {type: :actors, actors: remote_actors}.to_msgpack, 0)
+          when REMOTE_ASYNC
+            LibZMQ.msg_send(msg, remote_client(msg_val[1]), 0)
+          when REMOTE_ACTORS
+            LibZMQ.send(@pipe, [ACTORS, remote_actors].to_msgpack, 0)
           end
         rescue => e
-          LibZMQ.send(@pipe, {type: :exception, exception: e}.to_msgpack, 0)
+          LibZMQ.send(@pipe, [EXCEPTION, e].to_msgpack, 0)
         end
       end
     rescue => e
@@ -216,40 +221,31 @@ class Actor < ZMQ::Thread
 
     def handle_server
       peer, msg = @remote_server.recv
-      msg = MessagePack.unpack(msg.to_str(true))
+      msg = MessagePack.unpack(msg.to_str)
       begin
-        case msg.fetch(:type)
-        when :remote_new
-          instance = msg.fetch(:class).new(*msg.fetch(:args))
+        case msg.fetch(TYPE)
+        when REMOTE_NEW
+          instance = msg.fetch(2).new(*msg.fetch(3))
           id = instance.__id__
-          instance_msg = {
-            type: :instance,
-            object_id: id
-          }.to_msgpack
+          instance_msg = [INSTANCE, id].to_msgpack
           LibZMQ.msg_send(peer, @remote_server, LibZMQ::SNDMORE)
           LibZMQ.send(@remote_server, instance_msg, 0)
           @instances[id] = instance
-        when :remote_send
-          result = {
-            type: :result,
-            result: @instances.fetch(msg.fetch(:object_id)).__send__(msg.fetch(:method), *msg.fetch(:args))
-          }.to_msgpack
+        when REMOTE_SEND
+          result = [RESULT, @instances.fetch(msg.fetch(2)).__send__(msg.fetch(3), *msg.fetch(4))].to_msgpack
           LibZMQ.msg_send(peer, @remote_server, LibZMQ::SNDMORE)
           LibZMQ.send(@remote_server, result, 0)
-        when :remote_async
-          if (instance = @instances[msg[:object_id]])
+        when REMOTE_ASYNC
+          if (instance = @instances[msg[2]])
             begin
-              instance.__send__(msg.fetch(:method), *msg.fetch(:args))
+              instance.__send__(msg.fetch(3), *msg.fetch(4))
             rescue => e
               ZMQ.logger.crash(e)
             end
           end
         end
       rescue => e
-        exec_msg = {
-          type: :exception,
-          exception: e
-        }.to_msgpack
+        exec_msg = [EXCEPTION, e].to_msgpack
         LibZMQ.msg_send(peer, @remote_server, LibZMQ::SNDMORE)
         LibZMQ.send(@remote_server, exec_msg, 0)
       end
